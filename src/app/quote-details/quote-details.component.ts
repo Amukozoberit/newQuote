afd43b232815ce38d9a0983519357dddd95c1b9e
Amukozoberit <mwasheb@gmail.com>
2021-07-19 15:54:09 +0300

    added details.ts content

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { QuoteClass } from '../quote';
 
 @Component({
   selector: 'app-quote-details',
@@ -6,7 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-details.component.css']
 })
 export class QuoteDetailsComponent implements OnInit {
+  @Input() quote:QuoteClass=new QuoteClass("","",""); 
+  @Output() isComplete=new EventEmitter<boolean>();
+   @Output() comp=new EventEmitter<boolean>();
 
+   
+   delete(i:boolean){
+    this.isComplete.emit(i);
+    alert("heard");
+
+  }
+  addLinks(i:boolean){
+
+    if(i===true){
+    this.comp.emit(i);
+    }else{
+      this.comp.emit(i);
+    }
+      }
   constructor() { }
 
   ngOnInit(): void {
