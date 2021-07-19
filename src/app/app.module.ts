2b6fa80c29e20c2dea345c8c8c83694849eb8efb
Amukozoberit <mwasheb@gmail.com>
2021-07-19 15:45:47 +0300

    added quote-card,form componenst

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { QuoteCardComponent } from './quote-card/quote-card.component';
+import { QuotesFormComponent } from './quotes-form/quotes-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    QuoteCardComponent,
+    QuotesFormComponent
   ],
   imports: [
     BrowserModule,
