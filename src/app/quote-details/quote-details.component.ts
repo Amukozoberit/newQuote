4bdbeadba27750166fa229394265f00516285882
Amukozoberit <mwasheb@gmail.com>
2021-07-19 15:54:55 +0300

    commented error in quote-details

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { QuoteClass } from '../quote';
+// import { QuoteClass } from '../quote';
 
 @Component({
   selector: 'app-quote-details',
