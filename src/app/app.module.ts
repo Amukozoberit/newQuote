04258fa24bfacd8f3d9d60005632b1ae42df5c34
Amukozoberit <mwasheb@gmail.com>
2021-07-19 15:46:47 +0300

    added quote details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { QuoteCardComponent } from './quote-card/quote-card.component';
 import { QuotesFormComponent } from './quotes-form/quotes-form.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     QuoteCardComponent,
-    QuotesFormComponent
+    QuotesFormComponent,
+    QuoteDetailsComponent
   ],
   imports: [
     BrowserModule,
